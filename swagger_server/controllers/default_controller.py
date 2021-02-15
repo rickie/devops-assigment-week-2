d38c64e40651aded6ad5964a1599a961d9738cd2
Rick Ossendrijver <rick.ossies@hotmail.com>
2021-02-15 08:07:26 +0100

    Add extra line of explanation

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -57,7 +57,7 @@ def get_student_by_id(student_id, subject=None):  # noqa: E501
 def student_get(last_name=None):  # noqa: E501
     """Get student by last name
 
-     # noqa: E501
+     Returns a single student # noqa: E501
 
     :param last_name: The subject&#39;s last name
     :type last_name: str
